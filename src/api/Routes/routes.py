32ad28eea759cb79f977392b0a89e3ded5dfc96f
Ignacio <ignacio.vamo@gmail.com>
2024-07-27 02:24:08 -0500

    webhook

--- src/api/Routes/routes.py
+++ src/api/Routes/routes.py
@@ -1,7 +1,6 @@
 from src.api.Services.login_service import LoginService
 from src.api.Services.register_service import RegisterService
 from src.api.Services.update_service import UpdateService
-from src.api.Services.delete_service import DeleteService
 from ..Services.user_service import UserService
 from src.api.Services.grupo_service import GrupoService
 from src.api.Services.listar_materia_service import ListarMateriaId
@@ -13,8 +12,6 @@ def load_routes(api):
     api.add_resource(RegisterService, '/forum/register')
     # Método para actualizar un usuario
     api.add_resource(UpdateService, '/forum/update')
-    # Método para eliminar un usuario
-    api.add_resource(DeleteService, '/usuarios/delete_user')
     # Método para listar usuarios
     api.add_resource(UserService, '/user/list')
     # Método para agregar un grupo
